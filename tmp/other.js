23befe92d1b9cceb7744bedba1cd05d3b2a10b9b
mauudev <mtrigo143@gmail.com>
2024-06-16 21:25:23 -0400

    update dependencies and capi script

--- tmp/other.js
+++ tmp/other.js
@@ -7,61 +7,74 @@ import {
   ServerEvent as _ServerEvent,
   FacebookAdsApi,
 } from "facebook-nodejs-business-sdk";
-const Content = _Content;
+// const Content = _Content;
 const CustomData = _CustomData;
-const DeliveryCategory = _DeliveryCategory;
+// const DeliveryCategory = _DeliveryCategory;
 const EventRequest = _EventRequest;
 const UserData = _UserData;
 const ServerEvent = _ServerEvent;
-const data = [
-  {
-    action_source: "website",
-    event_id: 12345,
-    event_name: "MyTestEvent",
-    event_time: 1718385788,
-    user_data: {
-      client_ip_address: "254.254.254.254",
-      client_user_agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0",
-      em: "f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a",
-    },
-  },
-];
-const access_token =
-  "EAAGhTpVxZCKQBO4ew8y6F5ZBGIFqqZCGh7xBItl02TraZBZBVWgOaq1RY9cEQ1nDH7ZBC3yHkRoZBe8wALwJ6hOU0z1yTMzmZCCbZCXG4NFcIhGPZAoe76XX9P7KedMGhFkK55El5noJYc4FJaMuWHAs6q58CzvpZBZCKs3f7OXxYgVX4CYF07AkP6CNfsyGckdQw9g6PgZDZD";
-const pixel_id = "484978870726745";
-const api = FacebookAdsApi.init(access_token);
 
 let current_timestamp = Math.floor(new Date() / 1000);
+const access_token =
+  "EAAGhTpVxZCKQBOzdSQeX6FZCZBt1ieOOAZBpurfwGFZA7L5MjHgZAwegmnZCHZC0AQ7X86pKYwLsooRbIRtg6wl4Aiu1CyQCHWRVEh0jPtmOIfeK753SdWEjVdvtBv5Iur20z8xNb9daubxOWh5tdeCaC5NVal6bpu0OukKnviI6nWl7kOP54bi070nGSpx5tMFmZBwZDZD";
+const pixel_id = "25810445075237872";
+const api = FacebookAdsApi.init(access_token);
+const data = {
+  data: [
+    {
+      event_name: "TestEvent",
+      event_time: current_timestamp,
+      action_source: "email",
+      user_data: {
+        em: "f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a",
+        client_ip_address: "254.254.254.254",
+        client_user_agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0",
+      },
+    },
+  ],
+  test_event_code: "TEST63411",
+};
 
-const userData = new UserData()
+const userData_0 = new UserData()
+  .setEmails(["7b17fb0bd173f625b58636fb796407c22b3d16fc78302d79f0fd30c2fc2fc068"])
+  .setGender("M")
+  .setCountry("US")
+  .setPhones(["f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a"]);
+
+const userData_1 = new UserData()
+  .setEmails(["7b17fb0bd173f625b58636fb796407c22b3d16fc78302d79f0fd30c2fc2fc068"])
+  .setGender("M")
+  .setCountry("US")
+  .setFirstName("JohnSalchichon")
+  .setPhones(["f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a"]);
+
+const userData_2 = new UserData()
   .setEmails(["joe@eg.com"])
   .setPhones(["12345678901", "14251234567"])
   // It is recommended to send Client IP and User Agent for Conversions API Events.
-  // .setClientIpAddress(request.connection.remoteAddress)
-  // .setClientUserAgent(request.headers["user-agent"])
-  .setClientIpAddress("254.254.254.254")
-  .setClientUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0")
+  .setClientIpAddress("4.4.4.4")
+  .setClientUserAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0")
   .setFbp("fb.1.1558571054389.1098115397")
   .setFbc("fb.1.1554763741205.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890");
 
-const content = new Content()
-  .setId("product123")
-  .setQuantity(1)
-  .setDeliveryCategory(DeliveryCategory.HOME_DELIVERY);
-
-const customData = new CustomData().setContents([content]).setCurrency("usd").setValue(123.45);
-
-const serverEvent = new ServerEvent()
-  .setEventId(123456789)
+const customData_0 = new CustomData().setValue(142.52).setCurrency("USD");
+const purchaseEvent1 = new ServerEvent()
   .setEventName("Purchase")
   .setEventTime(current_timestamp)
-  .setUserData(userData)
-  .setCustomData(customData)
-  .setEventSourceUrl("http://jaspers-market.com/product/123")
+  .setUserData(userData_2)
+  // .setCustomData(customData_0)
   .setActionSource("website");
 
-const eventsData = [serverEvent];
-const eventRequest = new EventRequest(access_token, pixel_id).setEvents(eventsData);
+const viewContentEvent = new ServerEvent()
+  .setEventName("ViewContent")
+  .setEventTime(current_timestamp)
+  .setUserData(userData_2)
+  .setActionSource("website");
+
+const eventsData = [viewContentEvent];
+
+// const eventRequest = new EventRequest(access_token, pixel_id, eventsData, "TEST63411");
+const eventRequest = new EventRequest(access_token, pixel_id, eventsData);
 
 eventRequest.execute().then(
   (response) => {
