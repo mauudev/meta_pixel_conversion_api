55090120d2d7bfb76f4e8523bdaa4865e1fb2aee
mauudev <mtrigo1432@gmail.com>
2024-06-19 13:53:15 -0400

    add info and refactors

--- tmp/main.ts
+++ tmp/main.ts
@@ -8,12 +8,15 @@ import {
   FacebookAdsApi,
 } from "facebook-nodejs-business-sdk";
 import crypto from "crypto";
-import { pixelId, metaAccessToken } from "./envvars";
 
 const CustomData = _CustomData;
 const EventRequest = _EventRequest;
 const UserData = _UserData;
 const ServerEvent = _ServerEvent;
+
+const META_ACCESS_TOKEN = "<META_ACCESS_TOKEN>";
+const PIXEL_ID = "<PIXEL_ID>";
+const TEST_EVENT_CODE = "<TEST_EVENT_CODE>";
 
 const createHash = (data: string): string => {
   return crypto.createHash("sha256").update(data, "utf-8").digest("hex");
@@ -21,22 +24,7 @@ const createHash = (data: string): string => {
 
 let current_timestamp: number = Math.floor(new Date().getTime() / 1000);
 
-const api = FacebookAdsApi.init(metaAccessToken);
-const data = {
-  data: [
-    {
-      event_name: "TestEvent",
-      event_time: current_timestamp,
-      action_source: "email",
-      user_data: {
-        em: "f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a",
-        client_ip_address: "254.254.254.254",
-        client_user_agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0",
-      },
-    },
-  ],
-  test_event_code: "TEST63411",
-};
+const api = FacebookAdsApi.init(META_ACCESS_TOKEN);
 
 const userData_0 = new UserData()
   .setEmail(createHash("mtrigo143@gmail.com"))
@@ -46,20 +34,12 @@ const userData_0 = new UserData()
   .setClientIpAddress("4.4.4.4")
   .setClientUserAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0");
 
-const userData_2 = new UserData()
-  .setEmails(["joe@eg.com"])
-  .setPhones(["f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a"])
-  .setGender("M")
-  .setClientIpAddress("4.4.4.4")
-  .setClientUserAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0")
-  .setFbp("fb.1.1558571054389.1098115397")
-  .setFbc("fb.1.1554763741205.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890");
-
 const customData_0 = new CustomData().setValue(142.52).setCurrency("USD");
 const purchaseEvent1 = new ServerEvent()
-  .setEventName("Luquito's Event 2")
+  .setEventName("Purchase")
   .setEventTime(current_timestamp)
   .setUserData(userData_0)
+  .setCustomData(customData_0)
   .setActionSource("website");
 
 const viewContentEvent = new ServerEvent()
@@ -70,10 +50,7 @@ const viewContentEvent = new ServerEvent()
 
 const eventsData = [viewContentEvent, purchaseEvent1];
 
-const eventRequest = new EventRequest(metaAccessToken, pixelId, eventsData, undefined, "TEST63411");
-
-console.log(`META_ACCESS_TOKEN: ${metaAccessToken}`);
-console.log(`PIXEL_ID: ${pixelId}`);
+const eventRequest = new EventRequest(META_ACCESS_TOKEN, PIXEL_ID, eventsData, undefined, TEST_EVENT_CODE);
 
 eventRequest.execute().then(
   (response: any) => {
---
+++ tmp/meta-flow.md
@@ -0,0 +1,57 @@
+# Main flow of the entire integration
+
+## Introduction
+
+The entire flow of the integration is divided into the following main parts:
+
+- Frontend side
+- MetaConversions Bus & MetaConversions API
+- Admin management
+- Data Access Layer
+- Workers
+
+## Frontend side
+
+Divided into 2 main parts:
+
+- Meta Pixel integration
+- MetaConversions API integration via custom hook
+
+### Meta Pixel integration
+
+Using the npm package (https://www.npmjs.com/package/react-facebook-pixel), the Meta Pixel integration is done by adding the Meta Pixel script to the website. This script is responsible for tracking events on the website and sending them to MetaConversions API.
+
+### MetaConversions API integration via custom hook
+
+A custom hook is created to handle the integration with MetaConversions API which is integrated in multiple components of the website.
+This hook is responsible for sending events to the MetaConversions Bus, which then sends them to the MetaConversions API.
+
+## MetaConversions Bus & MetaConversions API
+
+A library created using the meta sdk (https://www.npmjs.com/package/facebook-nodejs-business-sdk) is used to send events to MetaConversions API and exposed in a custom hook.
+
+A new database table is created for storing events that are not delivered to MetaConversions API. This table is used to store events that are not delivered due to network issues or other reasons.
+If the sending process fails after a few retries, the event is marked as `not_delivered` and stored in the database. The `useEventBus` hook is responsible for retrying the sending process.
+
+## Admin management
+
+An admin panel is created to manage the events that are being to sent to MetaConversions API.
+The admin panel will have the following features:
+- The list of all Standard/Custom Events that are being sent to MetaConversions API
+- Enable/Disable the sending of specific events
+- Configurations settings to manage the sending process
+- Retry sending events that are not delivered
+- View the status of events that are being sent
+
+## Data Access Layer
+
+A new database table is created to store events that are being tracked to MetaConversions API.
+This table is used to store events that are being sent to MetaConversions API and their status.
+Before sending an event, we verify the status of the event, if its enabled or disabled to be sent.
+When a event is sent to MetaConversions API, it is marked as `delivered` in the database.
+
+## Workers
+
+A worker is created to handle the sending of events to MetaConversions API.
+This worker is responsible for sending events that are stored in the database and have not been delivered.
+The worker runs in the background and checks for events that are not delivered and retries sending them.
