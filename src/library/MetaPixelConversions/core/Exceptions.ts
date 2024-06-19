3eb688041f708ad57191a7cddaea861ddf31fe11
mauudev <mtrigo143@gmail.com>
2024-06-18 21:52:30 -0400

    add framework exceptions

---
+++ src/library/MetaPixelConversions/core/Exceptions.ts
@@ -0,0 +1,70 @@
+/**
+ * BaseExceptions
+ */
+export class BaseException extends Error {
+  constructor(message?: string) {
+    super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
+
+/**
+ * Bus exceptions
+ */
+export class MetaEventBusException extends BaseException {
+  constructor(message?: string) {
+    super(message || "MetaEventBusException");
+  }
+}
+
+/**
+ * Handler exceptions
+ */
+
+export class EventHandlerException extends BaseException {
+  constructor(message?: string) {
+    super(message || "EventHandlerException");
+  }
+}
+
+/**
+ * MetaClient exceptions
+ */
+export class MetaClientException extends BaseException {
+  constructor(message?: string) {
+    super(message || "MetaClientException");
+  }
+}
+
+export class MetaClientInitializationError extends MetaClientException {
+  constructor(message?: string) {
+    super(message || "MetaClientInitializationError");
+  }
+}
+
+/**
+ * Event exceptions
+ */
+export class EventException extends BaseException {
+  constructor(message?: string) {
+    super(message || "EventException");
+  }
+}
+
+export class EventCreationError extends EventException {
+  constructor(message?: string) {
+    super(message || "EventCreationError");
+  }
+}
+
+/**
+ * Standard event exceptions
+ *
+ * PurchaseEvent exceptions
+ */
+
+export class InvalidPurchaseEventData extends EventException {
+  constructor(message?: string) {
+    super(message || "InvalidPurchaseEventData");
+  }
+}
