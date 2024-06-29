eaaf63e17072c16b017b85be3fe473feb2b61897
mauudev <mtrigo143@gmail.com>
2024-06-29 15:37:09 -0400

    add meta error codes

---
+++ src/library/MetaPixelConversions/core/ErrorCodes.ts
@@ -0,0 +1,268 @@
+export const META_ERROR_CODES = {
+  metaServerErrors: {
+    "1": {
+      subcodes: ["99"],
+      description: "Unknown error",
+    },
+    "5000": {
+      subcodes: [],
+      description: "Unknown error code",
+    },
+    "3910001": {
+      subcodes: [],
+      description: "Account issue, try later",
+    },
+  },
+  metaRequestLimitErrors: {
+    "4": {
+      subcodes: [],
+      description: "Application request limit reached",
+    },
+    "17": {
+      subcodes: [],
+      description: "User request limit reached",
+    },
+    "1404078": {
+      subcodes: [],
+      description: "Temporarily blocked",
+    },
+    "1404163": {
+      subcodes: [],
+      description: "Ad permissions revoked",
+    },
+    "2859015": {
+      subcodes: [],
+      description: "Temporarily blocked",
+    },
+  },
+  metaRequestErrors: {
+    "10": {
+      subcodes: [],
+      description: "No permission for action",
+    },
+    "100": {
+      subcodes: ["33", "1487694", "1752129"],
+      description: "Invalid parameter",
+    },
+    "102": {
+      subcodes: [],
+      description: "Invalid session key",
+    },
+    "104": {
+      subcodes: [],
+      description: "Incorrect signature",
+    },
+    "190": {
+      subcodes: [],
+      description: "Invalid OAuth token",
+    },
+    "200": {
+      subcodes: ["1870034", "1870047"],
+      description: "Permission error",
+    },
+    "294": {
+      subcodes: [],
+      description: "Requires ads_management",
+    },
+    "2606": {
+      subcodes: [],
+      description: "Ad preview error",
+    },
+    "2607": {
+      subcodes: [],
+      description: "Invalid currency",
+    },
+    "2615": {
+      subcodes: [],
+      description: "Invalid adaccount call",
+    },
+    "2654": {
+      subcodes: ["1713092"],
+      description: "Custom audience error",
+    },
+    "1373054": {
+      subcodes: [],
+      description: "No call to action",
+    },
+    "1487007": {
+      subcodes: [],
+      description: "Ad edit error, end date",
+    },
+    "1487033": {
+      subcodes: [],
+      description: "Campaign end date error",
+    },
+    "1487056": {
+      subcodes: [],
+      description: "Ad set deleted",
+    },
+    "1487472": {
+      subcodes: [],
+      description: "Unpromotable content",
+    },
+    "1487566": {
+      subcodes: [],
+      description: "Campaign deleted",
+    },
+    "1487678": {
+      subcodes: [],
+      description: "App OS mismatch",
+    },
+    "1487831": {
+      subcodes: [],
+      description: "Product set load error",
+    },
+    "1487929": {
+      subcodes: [],
+      description: "Ambiguous object",
+    },
+    "1487990": {
+      subcodes: [],
+      description: "Too many archived adgroups",
+    },
+    "1815199": {
+      subcodes: [],
+      description: "Instagram access error",
+    },
+    "1815629": {
+      subcodes: [],
+      description: "Duplicate ad assets",
+    },
+    "1815694": {
+      subcodes: [],
+      description: "Permission error",
+    },
+    "1870088": {
+      subcodes: [],
+      description: "Connection targeting deprecated",
+    },
+    "1870090": {
+      subcodes: [],
+      description: "Agree to Custom Audience terms",
+    },
+    "1870092": {
+      subcodes: [],
+      description: "Agree to Business Tools terms",
+    },
+    "1870165": {
+      subcodes: [],
+      description: "Targeting age restriction",
+    },
+    "1870199": {
+      subcodes: [],
+      description: "Location targeting update",
+    },
+    "1885029": {
+      subcodes: [],
+      description: "Page mismatch",
+    },
+    "1885088": {
+      subcodes: [],
+      description: "Ad archived, edit name only",
+    },
+    "1885183": {
+      subcodes: [],
+      description: "App in development mode",
+    },
+    "1885204": {
+      subcodes: [],
+      description: "Set automatic bid",
+    },
+    "1885272": {
+      subcodes: [],
+      description: "Budget too low",
+    },
+    "1885557": {
+      subcodes: [],
+      description: "Unavailable post",
+    },
+    "1885650": {
+      subcodes: [],
+      description: "Budget too low",
+    },
+    "2238055": {
+      subcodes: [],
+      description: "Invalid Instagram parameters",
+    },
+    "2446149": {
+      subcodes: [],
+      description: "Invalid Instagram parameters",
+    },
+    "2446173": {
+      subcodes: [],
+      description: "Invalid target rule label",
+    },
+    "2446289": {
+      subcodes: [],
+      description: "Unavailable post",
+    },
+    "2446347": {
+      subcodes: [],
+      description: "Use existing post flag error",
+    },
+    "2446383": {
+      subcodes: [],
+      description: "External URL required",
+    },
+    "2446394": {
+      subcodes: [],
+      description: "Invalid detailed targeting",
+    },
+    "2446509": {
+      subcodes: [],
+      description: "Invalid campaign destination",
+    },
+    "2446580": {
+      subcodes: [],
+      description: "Invalid interactive components",
+    },
+    "2446712": {
+      subcodes: [],
+      description: "Store visits optimization deprecated",
+    },
+    "2446867": {
+      subcodes: [],
+      description: "Advantage+ campaign limit reached",
+    },
+    "2446880": {
+      subcodes: [],
+      description: "WhatsApp number disconnected",
+    },
+    "2490085": {
+      subcodes: [],
+      description: "Invalid crop key",
+    },
+    "2490155": {
+      subcodes: [],
+      description: "Post not available",
+    },
+    "2490372": {
+      subcodes: [],
+      description: "Shop destination required",
+    },
+    "2490427": {
+      subcodes: [],
+      description: "Ad rejected, update needed",
+    },
+    "2490468": {
+      subcodes: [],
+      description: "Ad rejected, update needed",
+    },
+    "2708008": {
+      subcodes: [],
+      description: "Unauthorized political ads",
+    },
+    "3858064": {
+      subcodes: [],
+      description: "Targeting age restriction",
+    },
+    "3858082": {
+      subcodes: [],
+      description: "Standard Enhancements missing",
+    },
+    "3858152": {
+      subcodes: [],
+      description: "Beneficiary info required",
+    },
+  },
+};
