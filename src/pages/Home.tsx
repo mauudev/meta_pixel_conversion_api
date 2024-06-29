d142e01d1f03c46abb9696fb4470d96aab431ebc
mauudev <mtrigo143@gmail.com>
2024-06-29 16:31:28 -0400

    integrate add to cart button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
+import { AddToCartButton } from "../components/AddToCartButton";
+
 const Home = () => {
   return (
     <div id="Home" style={{ height: "800px" }}>
       <h1 style={{ fontSize: "50px", padding: "20%" }}>Home</h1>
+      <AddToCartButton product={{ id: "123", name: "Product Name" }} />
     </div>
   );
 };
